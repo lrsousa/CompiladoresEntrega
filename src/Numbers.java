7915994f2984bc331ef6394483152eb20e3bd6fd
rsleonardo <leonarrangel@gmail.com>
2016-09-04 22:02:10 -0300

    upload arquivos segunda entrega

--- src/Numbers.java
+++ src/Numbers.java
@@ -1,6 +1,8 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.math.BigDecimal;
+import java.util.Stack;
 
 import org.antlr.v4.runtime.*; // class ANTLRInputStream , Token
 
@@ -8,53 +10,118 @@ import parser.NumbersLexer;
 
 public class Numbers {
     public static void main(String[] args) throws FileNotFoundException {
+    	Token tk;
         NumbersLexer lexer;
-        Token tk;
+        InputStream is = new FileInputStream("arquivo.kenai");
         
-        // Cria instancia do lexer criado pelo ANTLR
-        
-        InputStream is;
-        FileInputStream fis = new FileInputStream("arquivo.kenai");
-        is = fis;
+        Stack<Number> pilha = new Stack<Number>();
         
         try {
             lexer = new NumbersLexer(new ANTLRInputStream( is ));
             is.close();
         } catch (Exception e) {
-            // Pikachu!
+            // Charmander!
             System.out.println("Erro:" + e);
             System.exit(1);
             return;
         }
 
-        // Le da entrada padrao ateh chegar digitar CTRL-D (Linux/Mac)
-        // ou CTRL-Z (Windows)
-
         do {
             tk = lexer.nextToken();
             switch(tk.getType()) {
                 case NumbersLexer.BINARY:
-                    System.out.println("bin: " + tk.getText());
+                    pilha.add(Long.parseLong(tk.getText().replace("b", ""), 2));
+                    System.out.println("$1 = " + pilha.lastElement());
                     break;
-
                 case NumbersLexer.DECIMAL:
-                    System.out.println("dec: " + tk.getText());
+                	pilha.add(Long.parseLong(tk.getText()));
+                	System.out.println("$1 = " + pilha.lastElement());
                     break;
-                    
                 case NumbersLexer.REAL:
-                    System.out.println("real: " + tk.getText());
+                	pilha.add(Double.parseDouble(tk.getText()));
+                	System.out.println("$1 = " + pilha.lastElement());
                     break;
-                    
                 case NumbersLexer.HEXADECIMAL:
-                    System.out.println("hex: " + tk.getText());
+                	pilha.add(Long.parseLong(tk.getText().substring(2), 16));
+                	System.out.println("$1 = " + pilha.lastElement());
                     break;
-                
                 case NumbersLexer.EXPONENCIAL:
-                    System.out.println("exp: " + tk.getText());
+                	//pilha.add(new BigDecimal(tk.getText()).doubleValueExact());
+                	pilha.add(Double.valueOf(tk.getText()).doubleValue());
+                	System.out.println("$1 = " + pilha.lastElement());
                     break;
-                    
+                case NumbersLexer.SIMB_SOMA:
+                	calcula(pilha, 1);
+                	break;
+                case NumbersLexer.SIMB_SUBT:
+                	calcula(pilha, 2);
+                	break;
+                case NumbersLexer.SIMB_MULT:
+                	calcula(pilha, 3);
+                	break;
+                case NumbersLexer.SIMB_DIVS:
+                	calcula(pilha, 4);
+                	break;
+                case NumbersLexer.SIMB_EXPO:
+                	calcula(pilha, 5);
+                	break;
+                case NumbersLexer.COMANDO_STATUS:
+                	imprimirPilha(pilha);
+                	break;
+                case NumbersLexer.COMANDO_RESET:
+                	pilha = new Stack<Number>();
+                	System.out.println("reset");
+                	break;
             }
         } while (tk != null && tk.getType() != Token.EOF);
 
+    }
+    
+    static void imprimirPilha(Stack<Number> pilha) {
+    	for (int i = pilha.size() - 1; i >= 0; i--) {
+    		System.out.print("$" + (pilha.size() - i) + " = ");
+    		if(pilha.get(i).doubleValue()  % 1 == 0) System.out.println(pilha.get(i).longValue());
+    		else System.out.println(pilha.get(i).doubleValue());
+		}
+	}
+
+	static void calcula(Stack<Number> pilha, int operador) {
+    	if(pilha.size() < 2) {
+    		System.out.println("Pilha com menos de 2 valores numericos");
+    		return;
+    	}
+    	Number v1 = pilha.pop();
+    	Number v2 = pilha.pop();
+    	Number retorno = 0;
+    	switch(operador) {
+	    	case 1:
+	    		retorno = v2.doubleValue() + v1.doubleValue();
+	    		break;
+	    	case 2:
+	    		retorno = v2.doubleValue() - v1.doubleValue();
+	    		break;
+	    	case 3:
+	    		retorno = v2.doubleValue() * v1.doubleValue();
+	    		break;
+	    	case 4:
+	    		retorno = v2.doubleValue() / v1.doubleValue();
+	    		break;
+	    	case 5:
+	    		retorno = Math.pow(v2.doubleValue(), v1.doubleValue());
+	    		break;
+    	}
+    	
+    	if(Double.isInfinite((double) retorno)) {
+    		pilha.add(v2);
+    		pilha.add(v1);
+    		return;
+    	}
+    	
+    	if(retorno.doubleValue() % 1 == 0) {
+    		retorno =  retorno.longValue();
+    	}
+    	pilha.add(retorno);
+    	System.out.println("$1 = " + retorno.doubleValue());
+    	return;
     }
 }
